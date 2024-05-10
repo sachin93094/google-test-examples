3aa5dffd17ce2164b5f5b8c7c7639a087c056d63
sachin93094 <sachin93094@gmail.com>
2024-05-10 10:42:02 +0000

    updated o/p files

---
+++ CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runTests.dir/cpp_sorter.cpp.o"
+  "CMakeFiles/runTests.dir/cpp_sorter.cpp.o.d"
+  "CMakeFiles/runTests.dir/test/cpp_sorter_test.cpp.o"
+  "CMakeFiles/runTests.dir/test/cpp_sorter_test.cpp.o.d"
+  "runTests"
+  "runTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/runTests.dir/cpp_sorter.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/runTests.dir/cpp_sorter.cpp.o: \
+ /mnt/agent/work/1353a62248e06435/cpp_sorter.cpp \
+ /usr/include/stdc-predef.h \
+ /mnt/agent/work/1353a62248e06435/include/cpp_sorter.h
